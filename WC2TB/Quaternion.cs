8ff2f45c2fa56e136e198a048bd4b924193e7835
zgmrvn <zgmrvn+git@gmail.com>
2018-07-27 11:46:50 +0200

    better comment

--- WC2TB/Quaternion.cs
+++ WC2TB/Quaternion.cs
@@ -12,11 +12,12 @@ namespace Extensions
             float sqz = (float)Math.Pow(q.Z, 2);
             float sqw = (float)Math.Pow(q.W, 2);
 
+            // Yaw, Pitch, Roll 
             var euler = new Vector3
             (
-                (float)(Math.Asin(2 * q.X * q.Y - + 2 * q.Z * q.W) * (180.0 / Math.PI)),                       // Pitch 
-                (float)(Math.Atan2(2 * q.Y * q.W - 2 * q.X * q.Z, 1 - 2 * sqy - 2 * sqz) * (180.0 / Math.PI)), // Yaw
-                (float)(Math.Atan2(2 * q.X * q.W - 2 * q.Y * q.Z, 1 - 2 * sqx - 2 * sqz) * (180.0 / Math.PI))  // Roll 
+                (float)(Math.Asin(2 * q.X * q.Y - + 2 * q.Z * q.W) * (180.0 / Math.PI)),
+                (float)(Math.Atan2(2 * q.Y * q.W - 2 * q.X * q.Z, 1 - 2 * sqy - 2 * sqz) * (180.0 / Math.PI)),
+                (float)(Math.Atan2(2 * q.X * q.W - 2 * q.Y * q.Z, 1 - 2 * sqx - 2 * sqz) * (180.0 / Math.PI))
             );
 
             return euler;
